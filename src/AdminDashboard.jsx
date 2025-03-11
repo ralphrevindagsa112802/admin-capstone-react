697e99622452cc0c44e0d0bf96bab8d2ffb89c4f
Mark Angelo Gerodiaz <gerodiazmarkangelo02@gmail.com>
2025-03-12 00:41:21 +0800

    Update AdminDashboard.jsx

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -686,7 +686,7 @@ const AdminDashboard = () => {
                                                     <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 shadow-md rounded-lg z-50">
                                                         <p className="text-blue-600 text-center font-semibold py-2">Update Status</p>
                                                         <div className="flex flex-col">
-                                                            {["Pending", "Processing", "Out For Delivery", "Payment accepted", "Preparing", "Ready to pickup", "Out of delivery", "Completed", "Order accepted", "Cancelled"].map((status) => (
+                                                            {["Pending", "Processing", "Out For Delivery", "Ready to pickup", "Order Received", "Completed", "Cancelled"].map((status) => (
                                                                 <button
                                                                     key={status}
                                                                     onClick={() => handleStatusUpdate(order.orders_id, status)}
