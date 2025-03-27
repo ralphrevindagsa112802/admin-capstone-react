6b011dcdeaef06413663928d0e1d8e954ea467bb
Ralph Dagsa <ralphrevindagsa112802@gmail.com>
2025-03-27 19:19:57 +0800

    added React import

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from 'react-router-dom';
 import { FaEye } from 'react-icons/fa';
 import { FaEllipsisV } from "react-icons/fa";
